77d73bf059311ada1a4ad54c50053ef07943af9b
Thomas Schraitle <tom_schr@web.de>
2012-02-05 21:39:33 +0100

    Integrate XInclude support for jing

--- bin/db5validate.sh
+++ bin/db5validate.sh
@@ -6,6 +6,8 @@
 DBFILE="en/xml/DocBook-Cookbook.xml"
 OUTFILE=${BUILDDIR}/${DBFILE##*/}
 
-xmllint --xinclude --output ${OUTFILE}  ${DBFILE}
-jing -c ${FRAMEWORKSDIR}/rng/5.1/dbref.rnc ${OUTFILE} && \
+# xmllint --xinclude --output ${OUTFILE}  ${DBFILE}
+JING_FLAGS="-Dorg.apache.xerces.xni.parser.XMLParserConfiguration=org.apache.xerces.parsers.XIncludeParserConfiguration"
+
+ADDITIONAL_FLAGS="${JING_FLAGS}" jing -c ${FRAMEWORKSDIR}/rng/5.1/dbref.rnc ${OUTFILE} && \
   echo "Ok, document ${DBFILE} is valid."
