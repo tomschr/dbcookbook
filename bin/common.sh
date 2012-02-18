f5b50490334b982c2187dca7585da8b0934cb4b2
Thomas Schraitle <tom_schr@web.de>
2012-02-18 21:37:06 +0100

    Added more variables to make it more portable between Saxon6 and Saxon9

--- bin/common.sh
+++ bin/common.sh
@@ -6,7 +6,19 @@ ABSPATH=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 # Cut the last directory part, as script is in .../bin
 ABSPATH=${ABSPATH%/*}
 # 
-BUILDDIR="build"
+BUILDDIR=${ABSPATH}/build
 FRAMEWORKSDIR=${ABSPATH}/frameworks
+BASEXSLT2=${FRAMEWORKSDIR}/xslt2/base
+BASEXSLT1=${FRAMEWORKSDIR}/xslt/
+DBXSLT5="/usr/share/xml/docbook/stylesheet/nwalsh5/current"
+DBXSLT="/usr/share/xml/docbook/stylesheet/nwalsh/current"
 
-echo "Framework: $FRAMEWORKSDIR"
+XMLIN="en/xml/DocBook-Cookbook.xml"
+SINGLEHTMLOUT=${BUILDDIR}/html/DoCookBook.html
+CATALOGPROP="CatalogManager.properties"
+
+HLCONFIG="${DBXSLT5}/highlighting/xslthl-config.xml"
+HLFLAG="-Dxslthl.config=file://${HLCONFIG}"
+XINCLUDEFLAG="-Dorg.apache.xerces.xni.parser.XMLParserConfiguration=org.apache.xerces.parsers.XIncludeParserConfiguration"
+
+echo "Using Framework: $FRAMEWORKSDIR"
