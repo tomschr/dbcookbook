86606966cf5575e44c220f9e8067e8b90aea15d4
Thomas Schraitle <tom_schr@web.de>
2012-02-19 01:26:38 +0100

    Added function createEntity to create inclusions for ISO entities

--- bin/common.sh
+++ bin/common.sh
@@ -23,3 +23,46 @@ HLFLAG="-Dxslthl.config=file://${HLCONFIG}"
 XINCLUDEFLAG="-Dorg.apache.xerces.xni.parser.XMLParserConfiguration=org.apache.xerces.parsers.XIncludeParserConfiguration"
 
 echo "Using Framework: $FRAMEWORKSDIR"
+
+createEntity() {
+cat > ${BUILDDIR}/tmp/ents.ent <<EOF
+<!ENTITY % amsa.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-amsa.ent">
+%amsa.ent;
+<!ENTITY % amsb.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-amsb.ent">
+%amsb.ent;
+<!ENTITY % amsc.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-amsc.ent">
+%amsc.ent;
+<!ENTITY % amsn.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-amsn.ent">
+%amsn.ent;
+<!ENTITY % amso.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-amso.ent">
+%amso.ent;
+<!ENTITY % amsr.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-amsr.ent">
+%amsr.ent;
+<!ENTITY % box.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-box.ent">
+%box.ent;
+<!ENTITY % cyr1.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-cyr1.ent">
+%cyr1.ent;
+<!ENTITY % cyr2.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-cyr2.ent">
+%cyr2.ent;
+<!ENTITY % dia.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-dia.ent">
+%dia.ent;
+<!ENTITY % grk1.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-grk1.ent">
+%grk1.ent;
+<!ENTITY % grk2.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-grk2.ent">
+%grk2.ent;
+<!ENTITY % grk3.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-grk3.ent">
+%grk3.ent;
+<!ENTITY % grk4.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-grk4.ent">
+%grk4.ent;
+<!ENTITY % lat1.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-lat1.ent">
+%lat1.ent;
+<!ENTITY % lat2.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-lat2.ent">
+%lat2.ent;
+<!ENTITY % num.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-num.ent">
+%num.ent;
+<!ENTITY % pub.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-pub.ent">
+%pub.ent;
+<!ENTITY % tech.ent SYSTEM "/usr/share/xml/entities/xmlcharent/0.3/iso-tech.ent">
+%tech.ent;
+EOF
+}
