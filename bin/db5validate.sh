7db0ce505cdeda9c0002807b3b062e653b0bbef1
Thomas Schraitle <tom_schr@web.de>
2012-03-01 08:49:49 +0100

    - Fixed wrongly used variable to validate XML file - Support -h/--help option

--- bin/db5validate.sh
+++ bin/db5validate.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# set -x
+
+function usage() {
+echo -e "$0\nValidates DocBook 5 project files (with automatic XInclude resolution)\n"
+exit 1
+}
+
+
+case $1 in
+  -h|--help) usage;;
+esac
+
+
 [ -e common.sh ] && . common.sh
 [ -e bin/common.sh ] && . bin/common.sh
 
@@ -9,5 +22,5 @@ OUTFILE=${BUILDDIR}/${DBFILE##*/}
 # xmllint --xinclude --output ${OUTFILE}  ${DBFILE}
 JING_FLAGS="-Dorg.apache.xerces.xni.parser.XMLParserConfiguration=org.apache.xerces.parsers.XIncludeParserConfiguration"
 
-ADDITIONAL_FLAGS="${JING_FLAGS}" jing -c ${FRAMEWORKSDIR}/rng/5.1/dbref.rnc ${OUTFILE} && \
+ADDITIONAL_FLAGS="${JING_FLAGS}" jing -c ${FRAMEWORKSDIR}/rng/5.1/dbref.rnc ${DBFILE} && \
   echo "Ok, document ${DBFILE} is valid."
