3dc577819227babec1d69234a6183773593a863b
Thomas Schraitle <tom_schr@web.de>
2012-05-17 16:29:42 +0200

    Splitting and assembling a book with the assembly stylesheets

---
+++ en/xml/structure/assemblies/disassemble-book.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xsltproc --xinclude \
+   --stringparam assembly.filename assembly.xml \
+   --stringparam base.dir topics/ \
+   --stringparam chunk.section.depth 2 \
+   --stringparam chunk.first.sections 0 \
+   --stringparam manifest.in.base.dir 1 \
+   ../../../../frameworks/db-xslt-ns/assembly/topic-maker-chunk.xsl \
+   book.xml
