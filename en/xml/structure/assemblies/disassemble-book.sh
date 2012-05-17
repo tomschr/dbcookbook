b84e579756156d743013b63b99a7238c19c6d22f
Thomas Schraitle <tom_schr@web.de>
2012-05-17 20:52:45 +0200

    Removed chunk.first.sections parameter

--- en/xml/structure/assemblies/disassemble-book.sh
+++ en/xml/structure/assemblies/disassemble-book.sh
@@ -4,7 +4,6 @@ xsltproc --xinclude \
    --stringparam assembly.filename assembly.xml \
    --stringparam base.dir topics/ \
    --stringparam chunk.section.depth 2 \
-   --stringparam chunk.first.sections 0 \
    --stringparam manifest.in.base.dir 1 \
    ../../../../frameworks/db-xslt-ns/assembly/topic-maker-chunk.xsl \
    book.xml
