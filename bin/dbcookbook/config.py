cf3457626f8fe15ff8cb4bf8f11e6a159ef7e56f
Thomas Schraitle <tom_schr@web.de>
2014-04-26 16:59:20 +0200

    Added comments and data about SF project

--- bin/dbcookbook/config.py
+++ bin/dbcookbook/config.py
@@ -7,11 +7,22 @@ __version__ = "0.5"
 __docformat__ = "epytext"
 
 
-#
+# Where to write logfiles:
 LOGFILE='/var/tmp/%s.log' % os.path.splitext(os.path.basename(__package__))[0]
 
-#
+# Our main XML file:
 MAINFILE="en/xml/DocBook-Cookbook.xml"
 
-#
+# INI config file for configparser:
 CONFIGFILE=".dbcookbook.ini"
+
+# About this Sourceforge project:
+SFPROJECT={
+ 'user':    'tom_schr',
+ 'project': 'doccookbook',
+ 'site':    'frs.sourceforge.net',
+ # 'path':    '/home/frs/project/${SFPROJECT}',
+}
+SFPROJECT['path']='/home/frs/project/{project}'.format(**SFPROJECT)
+
+
