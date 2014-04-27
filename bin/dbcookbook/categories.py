e68690d1a7c2c69fb6b464f1abb9c671bd8e3689
Thomas Schraitle <tom_schr@web.de>
2014-04-27 19:21:29 +0200

    Implemented category transformation

--- bin/dbcookbook/categories.py
+++ bin/dbcookbook/categories.py
@@ -6,23 +6,20 @@ import os, os.path
 from .log import trace, logger
 from .config import config
 
+from lxml import etree
+
+
 @trace(logger)
 def resolvexinclude(parser, args=None):
     """Resolves XIncludes in main XML file and save the result into tempdir
     """
-    
-    from lxml import etree
-    
-    if not args or args.no_category:
-        return
-    
     logger.debug("Using {args.mainfile}".format(**locals()))
     tempdir=config.get('Common', 'tempdir')
     
     if not os.path.exists(tempdir):
         os.makedirs(tempdir)
         
-    xmlfile = os.path.basename(args.mainfile)
+    xmlfile = os.path.basename(args.mainfile)+".tmp"
     xmlparser = etree.XMLParser(
         # dtd_validation=False,  # use default (False)
         ns_clean=True)
@@ -33,8 +30,36 @@ def resolvexinclude(parser, args=None):
                 standalone=True,
               )
     return parser, args
-    
-def resolvecategories(parser, args=None):
-    resolvexinclude(parser, args)
 
+@trace(logger)
+def resolvecategories(parser, args=None):
+    basefile=os.path.basename(args.mainfile)
+    tempdir=config.get('Common', 'tempdir')
+    xmltmpfile = os.path.join(tempdir, basefile+".tmp")
+    xmlfile = os.path.join(tempdir, basefile)
+    
+    # We resolve any XIncludes and store it in the tempdir
+    resolvexinclude(parser, args)
+    
+    if not args or args.no_category:
+        # Should we use copy instead of rename?
+        os.rename(xmltmpfile, xmlfile)
+        logger.debug("No categories created; make {xmltmpfile} -> {xmlfile}".format(**locals()))
+        return parser, args
+
+    categoryxsl=config.get('XSLT1', 'categoryxsl')
+    xmldoc = etree.parse(xmltmpfile)
+    transform = etree.XSLT(etree.parse(categoryxsl))
+    
+    logger.debug("Transforming {xmltmpfile} with {categoryxsl}".format(**locals()))
+    resulttree = transform(xmldoc)
+    
+    logger.debug("-- Return from XSLT processor (Start) --")
+    for entry in transform.error_log:
+        logger.debug("{entry.message}".format(**locals()))
+    logger.debug("-- Return from XSLT processor (End) --")
+    
+    logger.debug("Writing transformation result to {xmlfile}".format(**locals()))
+    resulttree.write(xmlfile, encoding="utf-8", standalone=True,)
+    
 # EOF
