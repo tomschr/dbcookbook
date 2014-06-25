c4f30f0bcf4ad5a1b7340c0feb17081b7738ced9
Thomas Schraitle <tom_schr@web.de>
2014-06-26 01:38:07 +0200

    Implemented copyexamples

--- bin/dbcookbook/env.py
+++ bin/dbcookbook/env.py
@@ -52,6 +52,68 @@ def initenv(parser, args=None):
     except FileExistsError:
             pass
 
-    
+    copyexamples()
        
     os.chdir(pwd)
+
+def copyexamples():
+    from shutil import copytree, Error, ignore_patterns, copy2
+    from os.path import isfile, islink
+    from fnmatch import fnmatch
+
+    htmlbuilddir=os.path.normpath(config.get('Common', 'htmldir'))
+    xmldir=os.path.normpath(config.get('Common', 'xmldir'))
+    examples='examples'
+
+    logger.debug(" Trying to create {examples} directory".format(**locals()))
+    os.makedirs(os.path.join(htmlbuilddir, examples), exist_ok=True)
+
+    def mycopy(src, dst, *, follow_symlinks=True):
+        logger.info("Copying {src} -> {dst}".format(**locals()))
+        copy2(src, dst, follow_symlinks=follow_symlinks)
+
+    subdirs=getsubdirs()
+    for d in subdirs:
+        dest=os.path.join(htmlbuilddir, examples, d)
+        src=os.path.join(xmldir, d)
+
+        logger.debug(" Trying to copy {src} -> {dest}".format(**locals()))
+        try:
+            res = copytree(src, dest,
+                           copy_function=mycopy,
+                           ignore=ignore_patterns("*~", "*.bak", "*.orig", "defs.xml", "png") )
+        except FileExistsError as e:
+            logger.info("File already exists {e}".format(**locals()))
+        except Error as e:
+            logger.debug(" Shutils Error: {e}".format(**locals()))
+
+
+def getsubdirs():
+    """Get all directories which are used in our main XML file and below
+
+    Return found subdirs
+    """
+    from lxml import etree
+    from itertools import chain
+
+    namespaces={ 't': "urn:x-toms:docbook-ext",
+                 'd': "http://docbook.org/ns/docbook",
+                 'xl': "http://www.w3.org/1999/xlink" }
+    builddir=os.path.normpath(config.get('Common', 'builddir'))
+    tempdir=os.path.normpath(config.get('Common', 'tempdir'))
+    xmldir=os.path.normpath(config.get('Common', 'xmldir'))
+    mainfile=config.get('DEFAULT', 'mainbasefile')
+
+    xmlparser = etree.XMLParser(ns_clean=True)
+    tree = etree.parse(os.path.join(xmldir, mainfile), xmlparser)
+    tree.xinclude()
+
+    fn = tree.xpath("//t:output/t:filename", namespaces=namespaces)
+    xmlbase = [ i.xpath("//d:*/@xml:base", namespaces=namespaces)  for i in fn ]
+
+    # We use chain here to avoid nested lists (or in other words: we need a flatted lists)
+    res=set([ os.path.split(i)[0] for i in chain(*xmlbase) ])
+    logger.debug(" Found subdirectories: {res}".format(**locals()))
+
+    return res
+# EOF
